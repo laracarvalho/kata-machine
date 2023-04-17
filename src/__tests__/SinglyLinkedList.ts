9690f86adc7374d03d0cdb9ca6f61d674000da4d
laracarvalho <laraftoller@gmail.com>
2023-04-17 10:36:20 -0300

    feat: Singly Linked List

--- src/__tests__/SinglyLinkedList.ts
+++ src/__tests__/SinglyLinkedList.ts
@@ -1,7 +1,37 @@
 import SinglyLinkedList from "@code/SinglyLinkedList";
-import { test_list } from "./ListTest";
 
 test("linked-list", function () {
     const list = new SinglyLinkedList<number>();
-    test_list(list);
+    
+    list.append(5);
+    debugger;
+    list.append(7);
+    debugger;
+    list.append(9);
+    debugger;
+
+    expect(list.get(2)).toEqual(9);
+    console.log(list);
+    expect(list.removeAt(1)).toEqual(7);
+    console.log(list);    
+    expect(list.length).toEqual(2);
+
+    list.append(11);
+    console.log(list)
+    expect(list.removeAt(1)).toEqual(9);
+    debugger;
+    expect(list.remove(9)).toEqual(undefined);
+    expect(list.removeAt(0)).toEqual(5);
+    expect(list.removeAt(0)).toEqual(11);
+    expect(list.length).toEqual(0);
+
+    list.prepend(5);
+    list.prepend(7);
+    list.prepend(9);
+
+    expect(list.get(2)).toEqual(5);
+    expect(list.get(0)).toEqual(9);
+    expect(list.remove(9)).toEqual(9);
+    expect(list.length).toEqual(2);
+    expect(list.get(0)).toEqual(7);
 });
