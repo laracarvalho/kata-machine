848fde8a60cb2ebf6b9833ab5114c472c9619a5c
laracarvalho <laraftoller@gmail.com>
2023-04-17 10:40:26 -0300

    feat: BT In Order + PostOrder

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,19 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+  if (!curr) {
+    return path;
+  }
 
+  // pre-recurse
+  
+  // recurse
+  walk(curr.left, path);
+  walk(curr.right, path);
+  path.push(curr.value);
+  
+  //post-recurse
+  return path;
+}
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+  return walk(head, []);
 }
